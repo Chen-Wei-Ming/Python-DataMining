c0d24df2f68b8c33f99f4897ac95e694e826c1fe
ChenWeiMing <will19940625@gmail.com>
2016-11-18 16:18:30 +0800

    update
    update

---
+++ dataMining_HW3_pythonFormatSVM.py
@@ -0,0 +1,35 @@
+import os
+
+import pandas as pd
+import numpy as np
+import scipy as sp
+
+from IPython.display import Image
+
+from sklearn.model_selection import train_test_split as tts
+from sklearn.externals.six import StringIO
+from sklearn.metrics import accuracy_score , precision_score , recall_score , f1_score
+from sklearn.tree import export_graphviz
+from sklearn.datasets import dump_svmlight_file
+from sklearn import tree
+
+# import data from csv file
+df = pd.read_csv('data/character-deaths.csv')
+# transform NaN to 0
+df = df.fillna(0)
+# delete colums
+del df['Book of Death'] , df['Name'] , df['Death Chapter']
+
+# setting data value
+df['Death Year'] = df['Death Year'].apply(lambda death : 1 if death > 0 else 0)
+# df['Allegiances'] = df['Allegiances'].apply(lambda allegiances : 1 if allegiances == "")
+# setting end
+
+# settting colums
+attritube_colums = ['Allegiances' , 'Book Intro Chapter' , 'Gender' , 'Nobility' , 'GoT' , 'CoK' , 'SoS' , 'FfC' , 'DwD']
+# use setting colums to limit df colums
+attritube_df = df[list(attritube_colums)]
+attritube_df = pd.get_dummies(attritube_df)
+print(attritube_df)
+
+dump_svmlight_file(attritube_df,df['Death Year'],"output/deaths_svm.txt",zero_based=True,multilabel=False)
---
+++ dataMining_HW3_pytohnToSpark.py
@@ -0,0 +1,26 @@
+
+from pyspark.mllib.tree import DecisionTree, DecisionTreeModel
+from pyspark.mllib.util import MLUtils
+
+data = MLUtils.loadLibSVMFile(sc, 'deaths_svm_will.txt')
+(trainingData, testData) = data.randomSplit([0.75, 0.25])
+model = DecisionTree.trainClassifier(trainingData, numClasses=2, categoricalFeaturesInfo={},impurity='gini', maxDepth=5, maxBins=32)
+predictions = model.predict(testData.map(lambda x: x.features))
+labelsAndPredictions = testData.map(lambda lp: lp.label).zip(predictions)
+accuracy= labelsAndPredictions.filter(lambda (v, p): v == p).count() / float(testData.count())
+print('Accuracy = ' + str(accuracy))
+TP = float(labelsAndPredictions.filter(lambda (v, p): v == p and p == 1 ).count())
+TN = float(labelsAndPredictions.filter(lambda (v, p): v == p and p == 0 ).count())
+FP = float(labelsAndPredictions.filter(lambda (v, p): v != p and p == 1 ).count())
+FN = float(labelsAndPredictions.filter(lambda (v, p): v != p and p == 0 ).count())
+datasum = float(TP+TN+FP+FN)
+accuracyFormEvaluation = (TP + TN) / datasum
+precisionFormEvaluation = TP / (TP + FP)
+recallFormEvaluation = TP / (TP + FN)
+print('AccuracyFormEvaluation = ' + str(accuracyFormEvaluation))
+print('PrecisionFormEvaluation = ' + str(precisionFormEvaluation))
+print('RecallFormEvaluation = ' + str(recallFormEvaluation))
+# from pyspark.mllib.evaluation import MulticlassMetrics
+# metrics = MulticlassMetrics(labelsAndPredictions)\
+# metrics.precision()
+# metrics.recall()
